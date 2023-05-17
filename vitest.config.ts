c70ccea08b82dc06bfbdeb48ca51fee8bfd44f24
Hezekiah Hendry <hezekiah.hendry@gmail.com>
2023-05-17 15:25:41 -0400

    chore(vitest): fix `deps.inline`

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     deps: {
-      inline: true
+      inline: [/^(?!.*vitest).*$/]
     },
     globals: true,
     coverage: {
